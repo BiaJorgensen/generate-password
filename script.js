872a04a2701a5fce2e8a1b40301274e0f3dea3ec
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2022-12-17 15:43:23 -0600

    Created if statements in case the user chooses password lenght lower than 8 or higher than 128

--- script.js
+++ script.js
@@ -15,12 +15,23 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
     const a = prompt ("Choose a password lenght from 8 to 128 characters");
+    if (a<8) {
+      alert ("Please choose a password lenght higher than 8")
+    }
+    else if (a>128) {
+      alert ("Please choose a password lenght lower than 128")
+    }
+    else {
+      criteria ()
+    }
+}
+function criteria () {
     const b = confirm ("Would you like to include lowercase in your password?");
     const c = confirm ("Would you like to include upercase in your password?");
     const d = confirm ("Would you like to include numbers in your password?");
-    const e = confirm ("Would you like to include special characters in your passaword?");
+    const e = confirm ("Would you like to include special characters in your password?");
   
   }
 
-
+ 
 
