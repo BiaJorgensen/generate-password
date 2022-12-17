c7d2919409edd2162411fd028aaef1aa605063ef
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2022-12-17 15:23:38 -0600

    Created prompts that will show up in page after generate password button is clicked

--- script.js
+++ script.js
@@ -13,5 +13,14 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+    const a = prompt ("Choose a password lenght from 8 to 128 characters");
+    const b = confirm ("Would you like to include lowercase in your password?");
+    const c = confirm ("Would you like to include upercase in your password?");
+    const d = confirm ("Would you like to include numbers in your password?");
+    const e = confirm ("Would you like to include special characters in your passaword?");
+  
+  }
+
 
 
