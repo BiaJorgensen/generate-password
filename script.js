88488986cbafe6947f5a0b4feabb133ab6ce91ec
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2022-12-17 16:41:34 -0600

    Created variables for each of the criterias in case they the user wants to include them in the password

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
 function generatePassword() {
     const a = prompt ("Choose a password lenght from 8 to 128 characters");
     if (a<8) {
@@ -29,11 +31,34 @@ function generatePassword() {
 }
 function criteria () {
     const b = confirm ("Would you like to include lowercase in your password?");
+    if (b) {
+      const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      console.log(lowercase);
+    }
     const c = confirm ("Would you like to include upercase in your password?");
+    if (c) {
+      const upercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      console.log(upercase);
+    }
     const d = confirm ("Would you like to include numbers in your password?");
+    if (d) {
+      const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      console.log(number);
+    }
     const e = confirm ("Would you like to include special characters in your password?");
+    if (e) {
+      const symbol = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+      console.log (symbol);
+    }
+   
+
+
   
   }
+
+
+ 
+  
 
  
 
